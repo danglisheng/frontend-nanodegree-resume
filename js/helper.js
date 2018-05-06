3938c065c69f0636bbaba22874a4ed30d3198c8d
danglisheng <3182466719@qq.com>
2018-05-06 09:56:23 +0800

    first commit

--- js/helper.js
+++ js/helper.js
@@ -13,20 +13,20 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<span class="white-text">%data%</span><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">电话</span><span class="white-text">%data%</span></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
 var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">现居地</span><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
+var HTMLskillsStart = '<h3 id="skills-h3">技能一览</h3><ul id="skills" class="flex-column"></ul>';
 var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
@@ -47,49 +47,15 @@ var HTMLschoolName = '<a href="#">%data%';
 var HTMLschoolDegree = ' -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+var HTMLschoolMajor = '<em><br>专业: %data%</em>';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
+var HTMLonlineClasses = '<h3>培训经历</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
-var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
-
-
-/*
-The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
-*/
-$(document).ready(function() {
-  $('button').click(function() {
-    var $name = $('#name');
-    var iName = inName($name.text()) || function(){};
-    $name.html(iName);
-  });
-});
-
-/*
-The next few lines about clicks are for the Collecting Click Locations quiz in the lesson Flow Control from JavaScript Basics.
-*/
-var clickLocations = [];
-
-function logClicks(x,y) {
-  clickLocations.push(
-    {
-      x: x,
-      y: y
-    }
-  );
-  console.log('x location: ' + x + '; y location: ' + y);
-}
-
-$(document).click(function(loc) {
-  // your code goes here!
-});
-
-
 
 /*
 This is the fun part. Here's where we generate the custom Google Map for the website.
@@ -239,11 +205,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function(e) {
+ // Make sure the map bounds get updated on page resize
+ map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,156 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var $main = $("#main");
+var bio = {
+    name: "党黎圣",
+    role: "前端开发工程师",
+    contacts: {
+        mobile: "13260010971",
+        email: "3182466719@qq.com",
+        github: "https://github.com/danglisheng",
+        location: "北京"
+    },
+    welcomeMessage: "Hello",
+    biopic: "./images/fry.jpg",
+    skills: ["jQuery", "JavaScript", "CSS", "前端构建工具gulp"],
+    display: function() {
+        var $header = $("#header");
+        var $topContacts = $("#topContacts");
+				var $footerContacts=$("#footerContacts");
+        var formattedName = HTMLheaderName.replace("%data%", bio.name);
+        var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+        var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+        var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+        var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+        var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+        var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+        var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
+        $header.prepend(formattedRole);
+        $header.prepend(formattedName);
+       
+        function addContact(wrapper) {
+        wrapper.append(formattedMobile);
+        wrapper.append(formattedEmail);
+        wrapper.append(formattedGithub);
+        wrapper.append(formattedLocation);
+        }
+        addContact($topContacts);
+        addContact($footerContacts);
+        $header.append(formattedWelcomeMsg);
+        $header.append(formattedBioPic);
+
+        $header.append(HTMLskillsStart);
+        var $skillsSection = $("#skills");
+        $.each(bio.skills, function(index, skill) {
+            var formattedSkill = HTMLskills.replace("%data%", skill);
+            $skillsSection.append(formattedSkill);
+        });
+    }
+};
+bio.display();
+var work = {
+    jobs: [{
+        employer: "山西格盟国际能源有限公司",
+        title: "热工检修",
+        location: "山西省朔州市",
+        dates: "2015.07.01-2017.06.1"
+    }],
+    display: function() {
+    		var $workExperience = $("#workExperience");
+        work.jobs.forEach(function(job) {
+            $workExperience.append(HTMLworkStart);
+            var $workEntry = $(".work-entry:last");
+            var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", job.employer);
+            var formattedWorkTitle = HTMLworkTitle.replace("%data%", job.title);
+            $workEntry.append(formattedWorkEmployer + formattedWorkTitle);
+            var formattedWorkDates = HTMLworkDates.replace("%data%", job.dates);
+            $workEntry.append(formattedWorkDates);
+            var formattedWorkLocation = HTMLworkLocation.replace("%data%", job.location);
+            $workEntry.append(formattedWorkLocation);
+
+        });
+    }
+};
+work.display();
+var education = {
+    schools: [{
+        name: "长沙理工大学",
+        location: "湖南省长沙市",
+        degree: "学士学位",
+        majors: "测控技术与仪器",
+        dates: "2010.09.01-2015.06.23"
+    }],
+    onlineCourses: [{
+        title: "前端开发纳米学位",
+        school: "优达学城",
+        dates: "2018.3.21-",
+        url: "https://www.udacity.com/"
+    }],
+    display: function() {
+        var $education = $("#education");
+        education.schools.forEach(function(school) {
+            $education.append(HTMLschoolStart);
+            var $educationEntry = $(".education-entry:last");
+            var formattedSchoolName = HTMLschoolName.replace("%data%", school.name);
+            var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", school.degree);
+            $educationEntry.append(formattedSchoolName + formattedSchoolDegree);
+
+            var formattedSchoolDates = HTMLschoolDates.replace("%data%", school.dates);
+            $educationEntry.append(formattedSchoolDates);
+
+            var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", school.location);
+            $educationEntry.append(formattedSchoolLocation);
+
+            var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", school.majors);
+            $educationEntry.append(formattedSchoolMajor);
+        });
+        education.onlineCourses.forEach(function(course) {
+        	$education.append(HTMLschoolStart);
+          var $educationEntry = $(".education-entry:last");
+          $educationEntry.append(HTMLonlineClasses);
+          var formattedOnlineTitle=HTMLonlineTitle.replace("%data%",course.title);
+          var formattedOnlineSchool=HTMLonlineSchool.replace("%data%",course.school);
+          $educationEntry.append(formattedOnlineTitle+formattedOnlineSchool);
+          var formattedOnlineDates=HTMLonlineDates.replace("%data%",course.dates);
+          $educationEntry.append(formattedOnlineDates);
+          var formattedOnlineURL=HTMLonlineURL.replace("%data%",course.url);
+          $educationEntry.append(formattedOnlineURL);
+        });
+
+    }
+};
+education.display();
+var projects = {
+    projects: [{
+            title: "订阅阅读器",
+            dates: "2018.04.22-2018.04.25",
+            description: "点击页面左上角的汉堡菜单图标，会弹出信息源的列表，你可以选择你感兴趣的信息源。",
+            images: ["./images/feed-reader.jpg"]
+        },
+        {
+            title: "学生考勤表",
+            dates: "2018.05.03-2018.05.03",
+            description: "这是个考勤应用，复选框选中表示当日出勤，改变复选框的选中状态，缺勤天数和保存在本地存储的信息也会改变",
+            images: ["./images/attendance.jpg"]
+        }
+    ]
+};
+projects.display = function() {
+    var $projects = $("#projects");
+    projects.projects.forEach(function(project) {
+        $projects.append(HTMLprojectStart);
+        var $projectEntry = $(".project-entry:last");
+        var formattedProjectTitle = HTMLprojectTitle.replace("%data%", project.title);
+        $projectEntry.append(formattedProjectTitle);
+        var formattedProjectDates = HTMLprojectDates.replace("%data%", project.dates);
+        $projectEntry.append(formattedProjectDates);
+        var formattedProjectDescription = HTMLprojectDescription.replace("%data%", project.description);
+        $projectEntry.append(formattedProjectDescription);
+        project.images.forEach(function() {
+            var formattedProjectImage = HTMLprojectImage.replace("%data%", project.images);
+            $projectEntry.append(formattedProjectImage);
+        });
+
+    });
+}
+projects.display();
+
+$("#mapDiv").append(googleMap);
